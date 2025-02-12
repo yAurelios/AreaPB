5e04c02acc3cc164b1762bef828b45f4ccfc9f8b
Aurelios <jp.aureliostk@gmail.com>
2025-02-12 01:29:18 -0300

    v1.3
    Header, content, shortcut entre outros

--- src/js/footer.js
+++ src/js/footer.js
@@ -3,7 +3,8 @@ function loadFooter() {
     <footer>
         <article>
             <h2>Associação Regional de Engenheiros e Arquitetos de Pato Branco</h2>
-            <p>R. Tapajós, Nº305, Sala 106 - Centro, Pato Branco - PR, 85501-030 - (46) 99126-2389</p>
+            <br><br>
+            <p>R. Tapajós, Nº305, Sala 106 - Centro, Pato Branco - PR | CEP 85501-030 | (46) 99126-2389</p>
         </article>
     </footer>
     `;
