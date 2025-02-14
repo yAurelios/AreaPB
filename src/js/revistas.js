5f53e3ba7358bad3474d46946ca29324fc64e8ac
Aurelios <jp.aureliostk@gmail.com>
2025-02-13 21:47:23 -0300

    1.4.1
    Agora sim os redirects não bugaram no link final :D

--- src/js/revistas.js
+++ src/js/revistas.js
@@ -7,7 +7,7 @@ const magazines = [
         name: "Revista Engenharia em Debate | 3º Edição",
         description: "De uma parceria entre CREA-PR e AREA-PB, a atuação de diversas engenharias visando um futuro mais sustentável. Nessa 3º Edição, a Revista Engenharia em Debate trás conteúdos diversos, a retrospectiva de 2022/2023/2024, novos padrões de sustentabilidade e planejamento urbano. Confira!",
         date: "Ano 2025",
-        link: "/src/html/revistas/2024-2.html"
+        link: "/revista/2024-2"
     },
 ];
 
